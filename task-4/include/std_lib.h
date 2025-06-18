47a59f8ab3782f07341df9f1a68f0cce82235199
Kenzy-TC <marionapitupulu06@gmail.com>
2025-06-18 13:12:38 +0700

    Add files via upload

---
+++ task-4/include/std_lib.h
@@ -0,0 +1,19 @@
+#ifndef STD_LIB_H
+#define STD_LIB_H
+
+typedef unsigned char byte;
+typedef unsigned short word;
+typedef unsigned int dword;
+typedef enum { false, true } bool;
+
+int div(int a, int b);
+int mod(int a, int b);
+
+void memcpy(byte* src, byte* dst, unsigned int size);
+unsigned int strlen(char* str);
+bool strcmp(char* str1, char* str2);
+void strcpy(char* src, char* dst);
+char* strstr(const char* haystack, const char* needle);
+char* strchr(const char* str, int ch);void clear(byte* buf, unsigned int size);
+
+#endif
